4ea3b6fa465e0f5b5224b8ae08c962979093e355
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-05-08 01:48:45 +0900

    Lesson6を解答

--- src/pages/Lesson07/index.jsx
+++ src/pages/Lesson07/index.jsx
@@ -4,7 +4,43 @@ import Chart from "../../components/Chart07";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  //日付の形式とタイムゾーンの変換
+  for (const item of input) {
+    const d = new Date(`${item.createdAt} UTC`);
+    const year = d.getFullYear();
+    const month = `${d.getMonth() + 1}`.padStart(2, "0");
+    const date = `${d.getDate()}`.padStart(2, "0");
+    item.createdAt = `${year}-${month}-${date}`;
+  }
+
+  const dates = Array.from(new Set(input.map(({ createdAt }) => createdAt)));
+  dates.sort();
+
+  const count = { tweet: {}, retweet: {} };
+  for (const d of dates) {
+    count.tweet[d] = 0;
+    count.retweet[d] = 0;
+  }
+
+  for (const { createdAt, isRetweet } of input) {
+    if (isRetweet) {
+      count.retweet[createdAt] += 1;
+    } else {
+      count.tweet[createdAt] += 1;
+    }
+  }
+
+  return ["tweet", "retweet"].map((types) => {
+    return {
+      id: types,
+      data: dates.map((d) => {
+        return{
+          x: d,
+          y: count[types][d],
+        }
+      })
+    }
+  }); // ここを作りましょう！
 };
 
 const Lesson = () => {
