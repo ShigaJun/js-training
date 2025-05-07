374e1224a0b95cf9ad7464eae76228ccf02dc464
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-05-08 00:51:37 +0900

    Lesson6を解答

--- src/pages/Lesson06/index.jsx
+++ src/pages/Lesson06/index.jsx
@@ -4,7 +4,20 @@ import Chart from "../../components/Chart06";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const colors = {
+    男性: "blue",
+    女性: "red",
+  };
+
+  return input.map(({ gender, y, x }) => {
+    return {
+      color: colors[gender],
+      gender,
+      bmi: x / (y / 100) ** 2,
+      weight: x,
+      height: y,
+    };
+  }); // ここを作りましょう！
 };
 
 const Lesson = () => {
